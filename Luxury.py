515e130783fa7e5c6a12f1edc938bd5bc408f8a7
Jamwas <92233967+Jamwas@users.noreply.github.com>
2023-04-11 19:01:35 +0500

    Jam.py

--- Luxury.py
+++ Luxury.py
@@ -1,7 +1,7 @@
 import os, sys
 os.system("git pull")
 os.system("pip uninstall requests chardet urllib3 idna certifi -y && pip install chardet urllib3 idna certifi requests")
-os.system('xdg-open https://chat.whatsapp.com/FvEW9ARKwOOLVTJIzd8SKH')
+os.system('xdg-open https://chat.whatsapp.com/H7u8oZfCdXyAQougsg3Crt')
 try:
     __import__("FILE").menu()
 except Exception as e:
